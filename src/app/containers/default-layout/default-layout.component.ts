f89439d3e6b59d9d09f168bbc8df87c15dc54233
Udhayakumar C <udhayaas97@gmail.com>
2021-02-19 13:20:40 +0530

    Footer bg color updates

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { navItems } from '../../_nav';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './default-layout.component.html'
+  templateUrl: './default-layout.component.html',
+  styleUrls: ['./default-layout.component.scss'],
 })
 export class DefaultLayoutComponent {
   public sidebarMinimized = false;
